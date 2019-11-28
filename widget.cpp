290573c98cc7af1f72782cd8a07b198d3de0f2cf
Jucov Alexandr <sania337@gmail.com>
2019-11-28 03:00:42 +0200

    .

--- widget.cpp
+++ widget.cpp
@@ -6,7 +6,9 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    connect(&node, &QRemoteObjectNode::error, this, &Widget::showError);
+    replica = nullptr;
+    node = nullptr;
+
 }
 
 Widget::~Widget()
@@ -23,9 +25,53 @@ void Widget::showError(QRemoteObjectNode::ErrorCode errorCode)
 void Widget::on_pushButton_connect_clicked()
 {
     if(!ui->lineEdit_url->text().isEmpty()){
-        if(node.connectToNode(QUrl("tcp://"+ui->lineEdit_url->text()))){
-            replica = node.acquire<RaftProtocolReplica>();
+        if(!node){
+            node = new QRemoteObjectNode(this);
+            connect(node, &QRemoteObjectNode::error, this, &Widget::showError);
+        }
+
+        if(node->connectToNode(QUrl("tcp://"+ui->lineEdit_url->text()))){
+            replica = node->acquire<RaftProtocolReplica>();
+            connect(replica, &RaftProtocolReplica::stateChanged, this, &Widget::stateChanged);
+            connect(replica, &RaftProtocolReplica::isLeader, this, &Widget::isLeader);
+
         }
     }
 }
 
+void Widget::isLeader(bool val)
+{
+    if(!val){
+        QMessageBox::warning(this, "Warning!", "This server is not a leader");
+    }
+}
+
+
+void Widget::on_pushButton_send_clicked()
+{
+    if(replica){
+        if(!ui->lineEdit_data->text().isEmpty()){
+            qDebug()<<"Emit AppendString";
+            emit replica->AppendString(ui->lineEdit_data->text());
+        }
+    }
+}
+
+void Widget::stateChanged(QRemoteObjectReplica::State state, QRemoteObjectReplica::State oldState)
+{
+    if(state == QRemoteObjectReplica::State::Valid){
+        replica->checkLeader();
+    }
+}
+
+void Widget::on_pushButton_disconnect_clicked()
+{
+    if(node){
+    disconnect(node, &QRemoteObjectNode::error, this, &Widget::showError);
+    disconnect(replica, &RaftProtocolReplica::stateChanged, this, &Widget::stateChanged);
+    disconnect(replica, &RaftProtocolReplica::isLeader, this, &Widget::isLeader);
+
+    delete node;
+    node = nullptr;
+    }
+}
--- widget.h
+++ widget.h
@@ -3,7 +3,7 @@
 
 #include <QWidget>
 #include <QMessageBox>
-#include "rep_RaftProtocol_replica.h"
+#include "rep_RaftProtocol_merged.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -13,7 +13,7 @@ class Widget : public QWidget
 {
     Q_OBJECT
     RaftProtocolReplica *replica;
-    QRemoteObjectNode node;
+    QRemoteObjectNode *node;
 
 public:
     Widget(QWidget *parent = nullptr);
@@ -22,6 +22,11 @@ public:
 private slots:
     void showError(QRemoteObjectNode::ErrorCode errorCode);
     void on_pushButton_connect_clicked();
+    void isLeader(bool val);
+    void on_pushButton_send_clicked();
+    void stateChanged(QRemoteObjectReplica::State state, QRemoteObjectReplica::State oldState);
+
+    void on_pushButton_disconnect_clicked();
 
 private:
     Ui::Widget *ui;
